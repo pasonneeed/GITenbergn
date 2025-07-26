017231240bb1c3402e3a04decd5daa85187430a9
pasonneeed <chentao2690@126.com>
2025-07-26 10:06:39 +0800

    feat: #8 회원가입 퍼널 형식 구현

---
+++ src/hook/useFunnel.tsx
@@ -0,0 +1,28 @@
+import { ReactElement, ReactNode, useState } from 'react';
+
+export interface StepProps {
+  name: string;
+  children: ReactNode;
+}
+
+export interface FunnelProps {
+  children: Array<ReactElement<StepProps>>;
+}
+
+export const Step = (props: StepProps): ReactElement => {
+  return <>{props.children}</>;
+};
+
+export const useFunnel = (defaultStep: string) => {
+  const [step, setStep] = useState(defaultStep);
+
+  const Funnel = ({ children }: FunnelProps) => {
+    const targetStep = children.find(
+      (childStep) => childStep.props.name === step
+    );
+
+    return <>{targetStep}</>;
+  };
+
+  return { Funnel, setStep, currentStep: step } as const;
+};
