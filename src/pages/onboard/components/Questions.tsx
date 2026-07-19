5c2a66ab6c81cc259d5feb4c659450a51ff67d85
pasonneeed <chentao2690@126.com>
2026-07-19 13:23:20 +0800

    feat : [#13] 온보딩 질문 화면 컴포넌트화

---
+++ src/pages/onboard/components/Questions.tsx
@@ -0,0 +1,29 @@
+import OnboardCharacter from '@assets/images/onboarding.png';
+import OptionSelector from '@pages/onboard/components/OptionSelector';
+
+interface QuestionsProps {
+  question: string;
+  options: string[];
+  value: string;
+  onChange: (v: string) => void;
+}
+
+const Questions = ({ question, options, value, onChange }: QuestionsProps) => {
+  return (
+    <div className="flex w-full items-start gap-4">
+      <img
+        src={OnboardCharacter}
+        alt="캐릭터"
+        className="hidden h-[100px] w-[100px] shrink-0 md:block"
+      />
+
+      <div className="flex flex-1 flex-col space-y-8">
+        <h2 className="text-gray-900 font-T04-SB">{question}</h2>
+
+        <OptionSelector options={options} value={value} onChange={onChange} />
+      </div>
+    </div>
+  );
+};
+
+export default Questions;
