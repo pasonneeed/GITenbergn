cc41f45e78ce9ebb7876f73c28c2dbce9356912f
pasonneeed <chentao2690@126.com>
2026-08-28 02:52:00 +0800

    style : [#13] 온보딩 Step UI 수정

--- src/pages/onboard/components/Step.tsx
+++ src/pages/onboard/components/Step.tsx
@@ -12,10 +12,10 @@ const Step = ({ stepNumber, title, status }: StepProps) => {
   return (
     <div className="flex flex-col items-center space-y-2">
       {isCurrent && (
-        <div className="text-sm text-purple-500 font-B02-M">{title}</div>
+        <div className="text-sm text-purple-500 font-B03-M">{title}</div>
       )}
       <div
-        className={`flex h-12 w-12 items-center justify-center rounded-xl ${isPrevious ? 'bg-purple-100 text-purple-500' : ''} ${isCurrent ? 'bg-purple-500 text-white' : ''} ${status === 'next' ? 'bg-gray-200' : ''} `}
+        className={`flex h-8 w-8 items-center justify-center rounded-xl ${isPrevious ? 'bg-purple-100 text-purple-500' : ''} ${isCurrent ? 'bg-purple-500 text-white' : ''} ${status === 'next' ? 'bg-gray-200' : ''} `}
       >
         {isPrevious ? (
           <CheckIocn className="h-5 w-5" />
